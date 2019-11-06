dfb0bd424b13e6a684f2acec076a581501616e0b
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-11-06 23:36:46 +0530

    Added merging process
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app/merger.py
+++ app/merger.py
@@ -5,9 +5,14 @@ import timeit
 
 
 class Merger(object):
-  def __init__(self):
+  def __init__(self, read_base_dir, read_file_prefix, output_base_dir, output_file_prefix, max_file_size=1024):
     self.data = []
     self.merged_data = {}
+    self.read_base_dir = read_base_dir
+    self.read_file_prefix = read_file_prefix
+    self.output_base_dir = output_base_dir
+    self.output_file_prefix = output_file_prefix
+    self.max_file_size = max_file_size
 
   def read_files(self, base_dir, prefix):
     filenames = glob.glob(os.path.join(base_dir, prefix + '*.json'))
@@ -16,22 +21,31 @@ class Merger(object):
         json_file_data = json.load(json_file)
       self.data.append(json_file_data)
 
-  def write_file(self, output_dir, prefix):
-    pass
-
   def merge_files(self):
     for value in self.data:
       if self.merged_data:
         for key, single_data in value.items():
-          self.merged_data[key].extend(single_data)
+        	if isinstance(single_data, list):
+						self.merged_data[key].extend(single_data)
       else:
         self.merged_data.update(value)
 
+  def write_file(self, output_dir, prefix):
+    if not (os.path.isdir(output_dir)):
+    	# Using mkdir creates directory recursively
+    	os.makedirs(output_dir)
+  	counter = str(len(self.data) + 1)
+  	with open(os.path.join(output_dir, prefix + counter + '.json'), 'w') as output_file:
+  		json.dump(self.merged_data, output_file)
+
+  def execute(self):
+	  self.read_files(self.read_base_dir, self.read_file_prefix)
+	  self.merge_files()
+	  self.write_file(self.output_base_dir, self.output_file_prefix)
+
 
 def main():
-  merger = Merger()
-  merger.read_files('../data', 'data')
-  merger.merge_files()
-  print(merger.merged_data)
+  merger = Merger('../data', 'data', '../output', 'output')
+  merger.execute()
 
 main()
